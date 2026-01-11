d628d26d48386d7dcc898fac8fc34c75ffb496b5
우주 정 <145588575+wccjcc@users.noreply.github.com>
2026-01-11 16:53:38 +0900

    Create k번째수.java

---
+++ Java/Programmers/k번째수.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+class Solution {
+    public int[] solution(int[] array, int[][] commands) {
+        int[] answer = new int[commands.length];
+        
+        for (int i = 0 ; i < commands.length; i++){
+            int start = commands[i][0];
+            int end = commands[i][1];
+            int k = commands[i][2];
+            
+            int[] sliced_array = Arrays.copyOfRange(array,start-1,end);
+            Arrays.sort(sliced_array);
+            answer[i] = sliced_array[k-1];
+        }
+        return answer;
+    }
+}
